7eeb1a1e86ec7dadeff83b439bbcaf5d054a67a9
cinvenz <vincenzobortune@gmail.com>
2023-01-16 17:31:45 +0100

    create database and change .env

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function home()
+    {
+        return view('guest.home');
+    }
+
+    public function list()
+    {
+        $movies = Movie::all();
+        dd($movies);
+        return view('guest.list', compact('movies'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',      'HomeController@home')->name('home');
+Route::get('/list',  'HomeController@list')->name('list');
